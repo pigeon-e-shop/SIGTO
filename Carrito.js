a33805e384f6fa50e040aa48ea30bbe1ff93ca8f
gonzalezGaston <gastongonzalezuy@gmail.com>
2024-07-11 23:12:45 -0300

    Progreso en la funcion carrito y actualizacion de lo realizado

--- Carrito.js
+++ Carrito.js
@@ -1,28 +1,37 @@
-let articulos = [];
-let cantArticulos = 0;
+
+let articulos = JSON.parse(localStorage.getItem('articulos')) || [];
+let cantArticulos = articulos.length;
 $("#cantArticulos").html(cantArticulos);
-9
 
 
-function Articulos(id,nombre, precio, descripcion) {
+function Articulos(id, nombre, precio, descripcion) {
     this.id = id;
     this.nombre = nombre;
     this.precio = precio;
     this.descripcion = descripcion;
     this.mostrar = function () {
-        return this.nombre + " " + this.precio + ", Precio: " + this.descripcion + "<br>"+  "<br>";
+        return this.nombre + " " + this.precio + ", Precio: " + this.descripcion + "<br><br>";
     };
 }
-let pantalonNikeBlack = new Articulos(1,"Pantalon Nike Guay", 1000, "guay");
 
-let zapasNikeMola = new Articulos(2,"Zapas Nike Molona", 1300, "Mola");
+let pantalonNikeBlack = new Articulos(1, "Pantalon Nike Guay", 1000, "guay");
+let zapasNikeMola = new Articulos(2, "Zapas Nike Molona", 1300, "Mola");
 
-
-function agregarArt (articulo) {
+function agregarArt(articulo) {
     articulos.push(articulo);
-    cantArticulos++;
-    console.log(articulos)
+    let cantArticulos = articulos.length;
+    localStorage.setItem('articulos', JSON.stringify(articulos));
+    $("#cantArticulos").html(cantArticulos);
+    console.log(articulos) 
+}
+
+function eliminarTodosLosArticulos() {
+    articulos = [];
+    cantArticulos = 0;
+    localStorage.setItem('articulos', JSON.stringify(articulos));
     $("#cantArticulos").html(cantArticulos);
 }
 
-
+$("#addPantalon").click(() => agregarArt(pantalonNikeBlack));
+$("#addZapas").click(() => agregarArt(zapasNikeMola));
+$("#btnEliminarTodo").click(eliminarTodosLosArticulos);
