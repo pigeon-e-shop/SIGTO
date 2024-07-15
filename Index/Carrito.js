2f47f80d3f5047429a85a932eb8e65cea6d59511
gonzalezGaston <gastongonzalezuy@gmail.com>
2024-07-15 20:33:27 -0300

    finalizamos el carrito

--- Index/Carrito.js
+++ Index/Carrito.js
@@ -1,3 +1,14 @@
+let imagenes = {
+    1 : "../imagenesProductos/pantalon-nike-club-black.JPG",
+    2 : "../imagenesProductos/nike-vomero-17-black.JPG",
+    3 : "../imagenesProductos/camisetaPumaGris.JPG",
+    4 : "../imagenesProductos/mandoPs5.jpg",
+    5 : "../imagenesProductos/googlePlayCard50.JPG",
+    6 : "../imagenesProductos/usbMemory.JPG",
+    7 : "../imagenesProductos/lapizLabialRojo.JPG",
+    8 : "../imagenesProductos/rimel.JPG",
+    9 : "../imagenesProductos/lapizLabialAzul.JPG"
+}
 let articulos = JSON.parse(localStorage.getItem('articulos')) || [];
 let cantArticulos = Number(localStorage.getItem('cantArticulos')) || articulos.length;
 $("#cantArticulos").html(cantArticulos);
@@ -7,10 +18,21 @@ function Articulos(id, nombre, precio, descripcion) {
     this.nombre = nombre;
     this.precio = precio;
     this.descripcion = descripcion;
+  
+    
 }
 
-let pantalonNikeBlack = new Articulos(1, "Pantalon Nike Guay", 1000.50, "Guay");
-let zapasNikeMola = new Articulos(2, "Zapas Nike Molona", 1300.99, "Mola");
+let pantalonNikeBlack = new Articulos(1, "Pantalon Nike Guay", 25.50, "Guay");
+let zapasNikeMola = new Articulos(2, "Zapas Nike Molona", 79.99, "Mola");
+let camisetaPumaGris = new Articulos(3, "Camiseta Puma gris", 39.99, "100% Gris");
+let mandoPs5 = new Articulos(4, "mando Ps5", 79.99, "A jugar sin descanso");
+let googleCard50 = new Articulos(5, "Google Play Card 50 Dolar", 50, "50 dolar pal free");
+let usbMemory30 = new Articulos(6, "Usb Memory 30 Gb", 19.99, "Lo justo y necesario");
+let lapizLabialRojo = new Articulos(7, "Lapiz Labial Rojo", 9.99, "Rojo");
+let rimel = new Articulos(8, "rimel", 39.99, "Pestañas increibles");
+let lapizLabialAzul = new Articulos(9, "Lapiz Labial Azul", 19.99, "Azul");
+
+
 
 function agregarArt(articulo) {
     articulos.push(articulo);
@@ -39,7 +61,8 @@ function renderizarArticulos() {
     articulos.forEach((articulo, index) => {
         const item = `
             <li>
-                <span>${articulo.nombre} - $${articulo.precio}</span>
+                <img src="${imagenes[articulo.id]}" width="100" height="100" class="articuloImagen">
+                <span class="articuloTexto">${articulo.nombre} - $${articulo.precio}</span>
                 <button class="btn-remove" onclick="eliminarArticulo(${index})">Eliminar</button>
             </li>
         `;
