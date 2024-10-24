e02a829829a7c0b9a78f97e86ec2e75e5f075527
Heikki Laptop <santi.vazquez.rubio@gmail.com>
2024-10-24 00:21:08 -0300

    carrito.controller.php

---
+++ controller/carrito.controller.php
@@ -0,0 +1,54 @@
+<?php
+
+
+require_once('../model/Create.php');
+require_once('../model/Read.php');
+require_once('../model/Update.php');
+require_once('../model/Delete.php');
+
+header('Content-Type: application/json');
+
+$read = new Read();
+$create = new Create();
+$update = new Update();
+$delete = new Delete();
+
+$mode = $_POST['mode'];
+
+try {
+    switch ($mode) {
+        case 'agregar':
+            // agregar al carrito
+            $create->agregarCarrito($_POST['idCarrito'], $_POST['idArticulo'], $_POST['cantidad']);
+            break;
+
+        case 'eliminar':
+            // sacar del carrito
+            $delete->sacarDelCarrito($_POST['idArticulo'], $_POST['idCarrito']);
+            break;
+
+        case 'actualizar':
+            // cambiar cantidad
+            echo json_encode($update->editCantidadArticulosEnCarrito($_POST['cantidad'], $_POST['idCarrito'], $_POST['idArticulo']));
+            break;
+
+        case 'leer':
+            try {
+                $data = $read->getCarrito($_POST['idUsuario']);
+            
+                if (empty($data)) {
+                    echo json_encode(['status' => 'error', 'message' => 'El carrito está vacío.']);
+                } else {
+                    echo json_encode(['status' => 'success', 'data' => $data]);
+                }
+            } catch (Exception $e) {
+                echo json_encode(['status' => 'error', 'message' => $e->getMessage()]);
+            }
+
+
+        default:
+            throw new Exception("Error Processing Request", 1);
+    }
+} catch (Exception $e) {
+    return json_encode($e);
+}
