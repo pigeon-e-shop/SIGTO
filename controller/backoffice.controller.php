280226181d7cbb7d7945d9ec279050232917ef0c
Heikki-dotcom <santi.vazquez.rubio@gmail.com>
2024-10-27 23:45:27 -0300

    avance paginas de registro y login

--- controller/backoffice.controller.php
+++ controller/backoffice.controller.php
@@ -16,65 +16,77 @@ $delete = new Delete();
 switch ($_POST['mode']) {
     case 'getArticulos':
         echo json_encode($read->read_articulo_detalleByEmpresa(8));
-        break;
-    
-    case 'cambiarVisibilidad':
-        try {
-            if (!(isset($_POST['visibilidad']) && isset($_POST['idArticulo']))) {
-                throw new Exception("Error Processing Request", 1);
-            } else {
-                $data = ($update->cambiarVisibilidad($_POST['visibilidad'],$_POST['idArticulo']));
-                if ($data) {
-                    echo 'ok';
+        break;    
+
+        case 'cambiarVisibilidad':
+            try {
+                if (!(isset($_POST['visibilidad']) && isset($_POST['idArticulo']))) {
+                    throw new Exception("Error Processing Request", 1);
                 } else {
-                    echo 'bad';
+                    $data = $update->cambiarVisibilidad($_POST['visibilidad'], $_POST['idArticulo']);
+                    if ($data) {
+                        echo json_encode(['status' => 'success']);
+                    } else {
+                        echo json_encode(['status' => 'error', 'message' => 'Failed to change visibility']);
+                    }
                 }
+            } catch (Exception $e) {
+                echo json_encode(['status' => 'error', 'message' => $e->getMessage()]);
             }
-        } catch (Exception $e) {
-            echo json_encode($e);
-        }
-        break;
+            break;
 
     case 'getInfoArticulo':
-        try {
-            if (!(isset($_POST['idArticulo']))) {
-                throw new Exception("Error",1);
-            } else {
-                echo json_encode($read->readArticuloById($_POST['idArticulo']));
-            }
-        } catch (Exception $e) {
-            throw $e;
+    try {
+        if (!(isset($_POST['idArticulo']))) {
+            throw new Exception("Error: Articulo ID is required", 1);
+        } else {
+            echo json_encode($read->readArticuloById($_POST['idArticulo']));
         }
-        break;
+    } catch (Exception $e) {
+        echo json_encode(['status' => 'error', 'message' => $e->getMessage()]);
+    }
+    break;
+
 
     case 'getInfoEmpresa':
         try {
             if (!(isset($_POST['email']))) {
-                throw new Exception("Error Processing Request", 1);
+                throw new Exception("Error Processing Request: Email is required", 1);
             } else {
                 echo json_encode($read->readDetalleEmpresa($_POST['email']));
             }
         } catch (Exception $e) {
-            throw $e;
-        }
-        break;
-
-    case 'getComments':
-        try {
-            // check ID
-            if (!(isset($_POST['id']))) {
-                throw new Exception("Error Processing Request", 1);
-            } else {
-                echo json_encode($read->getCommentsByArticulo($_POST['id']));
-            }
-        } catch (Exception $e) {
-            throw $e;
+            echo json_encode(['status' => 'error', 'message' => $e->getMessage()]);
         }
         break;
     
-    case 'getViewsXMes':
-        echo json_encode($read->getvistasXMes($_POST['idArticulo']));
-        break;
+
+        case 'getComments':
+            try {
+                if (!(isset($_POST['id']))) {
+                    throw new Exception("Error Processing Request: Article ID is required", 1);
+                } else {
+                    echo json_encode($read->getCommentsByArticulo($_POST['id']));
+                }
+            } catch (Exception $e) {
+                echo json_encode(['status' => 'error', 'message' => $e->getMessage()]);
+            }
+            break;
+        
+    
+            case 'getViewsXMes':
+                try {
+                    if (!(isset($_POST['idArticulo']))) {
+                        throw new Exception("Error Processing Request: Article ID is required", 1);
+                    } else {
+                        echo json_encode($read->getvistasXMes($_POST['idArticulo']));
+                    }
+                } catch (Exception $e) {
+                    echo json_encode(['status' => 'error', 'message' => $e->getMessage()]);
+                }
+                break;
+            
+        
     default:
         echo json_encode(['status'=>'error','message'=>'default']);
         break;
